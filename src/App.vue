1a65da8348497e0886f5a9033cce17542346e6c2
pphatdev <leatsophat02@gmail.com>
2025-03-15 09:16:49 +0700

    📜: Hello World

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  // @ts-ignore
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  const msg = "Hello World"
+</script>
+
+<template>
+  <h1 class="w-full py-10 mx-auto text-2xl font-semibold text-center text-red-500">{{ msg }}</h1>
+</template>
