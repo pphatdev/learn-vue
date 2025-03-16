0f7464f6d02562839c5f38a50179fb0e46552624
pphatdev <leatsophat02@gmail.com>
2025-03-16 11:10:26 +0700

    Dynamic CSS Classes with Vue

--- src/components/Learning/DynamicClasses.vue
+++ src/components/Learning/DynamicClasses.vue
@@ -14,12 +14,12 @@ const fruits = ref([
         <h1 class="w-full text-4xl font-bold shrink-0">{{ msg }}</h1>
         <ul class="w-full my-5 border divide-y divide-gray-200 rounded-xl">
             <li class="px-4 py-1 font-medium cursor-pointer"
-                v-for="({ id, name }, index) in fruits"
-                :key="id"
-                :class="{ 'line-through text-red-500': fruits[index].checked, 'text-green-500': !fruits[index].checked }"
-                @click="fruits[index].checked = !fruits[index].checked"
+                v-for="(item, index) in fruits"
+                :key="item.id"
+                :class="{ 'line-through text-red-500': item.checked, 'text-green-500': !item.checked }"
+                @click="item.checked = !item.checked"
             >
-                {{ index + 1 }}. {{ name }}
+                {{ index + 1 }}. {{ item.name }}
             </li>
         </ul>
     </div>
